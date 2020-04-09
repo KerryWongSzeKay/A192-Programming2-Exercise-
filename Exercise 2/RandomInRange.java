042d69915fa6f18e52ce4faae4448459967ec209
KerryWongSzeKay <61194252+KerryWongSzeKay@users.noreply.github.com>
2020-04-09 16:00:36 +0800

    Add files via upload

--- Exercise 2/RandomInRange.java
+++ Exercise 2/RandomInRange.java
@@ -1,4 +1,4 @@
-/*import java.util.Scanner;
+import java.util.Scanner;
 import java.util.Random;//import scanner and random
 public class RandomInRange//start of class
 {
@@ -19,27 +19,7 @@ public class RandomInRange//start of class
       new Random(System.currentTimeMillis()).nextInt((upper-lower)+1);
       System.out.println("The Random Number : "+rng);//display random number
    }//end of constructor
-}//end of class*/
-
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
-
-public class RandomInRange {
-
-    public static void main(String[] args) {
-        //Q4
-        System.out.println("Enter 2 number: ");
-        Scanner input = new Scanner(System.in);
-        int num1=input.nextInt();
-        int num2=input.nextInt();
-        if(num1>num2){
-            System.out.println("0");
-        }else {
-            int randomInt = ThreadLocalRandom.current().nextInt(num1, num2 + 1);
-            System.out.println("Random integer between " + num1 + " and " + num2 + ": " + randomInt);
-        }
-    }
-}
+}//end of class
 
       
       
